317c77546f6e0189024af858165398f0e86085a9
FranciscoTxz <angelftv2002@hotmail.com>
2024-08-29 07:51:38 -0600

    Entitis Frameworks core and Add Data Context

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,14 @@
-//dotnet watch --no-hot-reload  
+ using API.Data;
+using Microsoft.EntityFrameworkCore; //dotnet watch --no-hot-reload 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
