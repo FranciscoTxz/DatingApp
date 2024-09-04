9b3334b78a3e0f3ffa53fa770b5c7e121459caf8
FranciscoTxz <angelftv2002@hotmail.com>
2024-09-04 07:59:13 -0600

    New app 4/09

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'DatingApp';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+      next: response => this.users = response,
+      error: error => console.error(error),
+      complete: () => console.log('HTTP request completed.')
+    });
+  }
 }
