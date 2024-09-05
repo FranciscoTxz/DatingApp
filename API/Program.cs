10065aa559691647a93a6b6ca6166391d5c57a39
FranciscoTxz <angelftv2002@hotmail.com>
2024-09-05 07:22:53 -0600

    parcial 01 - Cors 5/09

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+//Configure the hhtp request pipeline
+app.UseCors(policy => policy.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins(
+        "http://localhost:4200", 
+        "https://localhost:4200"));
 
 app.MapControllers();
 
