7605a5478d830481f2d8a52f069c99416659b07f
FranciscoTxz <angelftv2002@hotmail.com>
2024-09-19 07:15:58 -0600

    Parcial 01 - Login Ready

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -33,6 +33,32 @@ public class AccountController(DataContext context) : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> LoginAsync(LoginRequest request)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(x =>
+            x.UserName.ToLower() == request.username.ToLower());
+        
+        if (user == null)
+        {
+            return Unauthorized("Invalid username or password");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid username or password");
+            }
+        }
+
+        return user;
+
+    }
+
     private async Task<bool> UserExistsAsync(string username) =>
         await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
 }
