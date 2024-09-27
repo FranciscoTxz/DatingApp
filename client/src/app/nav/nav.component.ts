8f26063aae6bead5cec3cf331c9235cc2cb37357
A. Francisco T. <angelftv2002@hotmail.com>
2024-09-27 09:25:37 -0600

    Parcial 01 - Persist the login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,26 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BrowserAnimationsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 
 export class NavComponent {
-  private accountService = inject(AccountService);
-  loggedIn: boolean = false;
+  accountService = inject(AccountService);
   model: any = {};
 
   login(): void {
     this.accountService.login(this.model).subscribe({
       next: (response) => {
         console.log(response);
-        this.loggedIn = true;
       },
       error: (error) => {
         console.log("Se ha producido un error: " + error);
@@ -29,6 +27,6 @@ export class NavComponent {
   }
 
   logout(): void {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
