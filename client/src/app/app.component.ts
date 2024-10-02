4d283d929cec8984a3084f03ad478353b024a2f4
A. Francisco T. <angelftv2002@hotmail.com>
2024-10-02 07:53:15 -0600

    Parcial 01 - Parent to child organization

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './services/account.service';
 import { HomeComponent } from './home/home.component';
@@ -13,26 +12,17 @@ import { HomeComponent } from './home/home.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
   title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
+
   setCurrentUser(): void{
     const userString = localStorage.getItem("user");
     if (!userString) return;
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
-  }
-  getUsers() {
-    this.http.get<Object>("https://localhost:5001/api/users").subscribe({
-      next: (response) => { this.users = response; },
-      error: (error) => { console.log(error); },
-      complete: () => { console.log("Request completed!"); }
-    });
   }
 }
