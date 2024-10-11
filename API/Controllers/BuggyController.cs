82dbd1203d6c537d588cbc25f6a46440d7e47c00
FranciscoTxz <angelftv2002@hotmail.com>
2024-10-11 07:28:58 -0600

    Parcial 02 - Add Buggy Controller and Register

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+namespace API.Controllers;
+
+using API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+public class BuggyController(DataContext context): BaseApiController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<string> GetNotFound()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<string> GetServerError()
+    {
+        var result = context.Users.Find(-1)??
+            throw new ArgumentException("Server error ");
+        return "random text";
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest();
+    }
+
+
+
+}
