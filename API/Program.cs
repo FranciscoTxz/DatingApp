977e3131757a86e4265968ea7e4462cd5b795fa2
FranciscoTxz <angelftv2002@hotmail.com>
2024-10-18 07:11:51 -0600

    Parcial 02 - Seed Data

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middlewares;
+using Microsoft.EntityFrameworkCore;
 //dotnet watch --no-hot-reload 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,7 +23,20 @@ app.UseCors(policy => policy
         "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsersAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error ocurred during migration/seeding");
+}
 
 app.Run();
